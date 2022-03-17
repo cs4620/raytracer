89cd0e79925baab806a2898cdbb8387f7d39456b
B. Ricks <bricks@unomaha.edu>
2022-03-17 15:22:11 -0500

    Adding lines and vector code

--- Vector3.java
+++ Vector3.java
@@ -27,11 +27,36 @@ public class Vector3 {
     return x*other.x + y*other.y+z*other.z;
   }
 
+  //(a_y\cdot b_z-a_z\cdot b_y, a_z\cdot b_x-a_x\cdot b_z, a_x\cdot b_y - a_y\cdot b_x)
   public Vector3 cross(Vector3 other){
-    var x  = y*other.z-z*other.y;
-    var y = z*other.x-x*other.z;
-    var z = x*other.y-y*other.x;
-    return new Vector3(x,y,z);
+    var x1  = y*other.z-z*other.y;
+    var y1 = z*other.x-x*other.z;
+    var z1 = x*other.y-y*other.x;
+    return new Vector3(x1,y1,z1);
+  }
+
+  public boolean equals(Vector3 other){
+    return x == other.x && y == other.y && z == other.z;
+  }
+
+  public double absoluteDifference(Vector3 other){
+    return Math.abs(x - other.x) + Math.abs(y - other.y) + Math.abs(z - other.z);
+  }
+
+  public boolean nearlyEquals(Vector3 other){
+    return absoluteDifference(other) < .0001;
+  }
+
+  public Vector3 plus(Vector3 other){
+    return new Vector3(x+other.x, y+other.y, z+other.z);
+  }
+
+  public Vector3 minus(Vector3 other){
+    return new Vector3(x - other.x, y - other.y, z - other.z);
+  }
+
+  public Vector3 negate(){
+    return new Vector3(-x, -y, -z);
   }
 
 }
