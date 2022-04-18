0aa6c678ba8c1420b80d2f343e4b11c5db9c5609
B. Ricks <bricks@unomaha.edu>
2022-04-18 15:24:07 -0500

    Add finished code

--- 4.12.22 - Triangles/Beginning/Triangle.java
+++ 4.12.22 - Triangles/Beginning/Triangle.java
@@ -42,7 +42,27 @@ public class Triangle implements Geometry{
 
   @Override
   public TAndNormal intersect(Ray ray) {
-    return null;
+    if(ray.direction.x == 0 && ray.direction.y == 0 && ray.direction.z == 0){
+      System.out.println("Debug");
+    }
+    var planeTAndNormal = new Plane(this.points).intersect(ray);
+
+    var collisionPoint = ray.origin.plus(ray.direction.scale(planeTAndNormal.t));
+    var normal = planeTAndNormal.normal.normalize();
+
+    Plane plane1 = new Plane(points[1], points[0], points[1].plus(normal));
+    Plane plane2 = new Plane(points[2], points[1], points[2].plus(normal));
+    Plane plane3 = new Plane(points[0], points[2], points[0].plus(normal));
+
+    float distance1 = plane1.offset(collisionPoint);
+    float distance2 = plane2.offset(collisionPoint);
+    float distance3 = plane3.offset(collisionPoint);
+
+    if(distance1 < 0 || distance2 < 0 || distance3 < 0){
+      return new TAndNormal(-1, null);
+    }
+
+    return planeTAndNormal;
   }
 
   
